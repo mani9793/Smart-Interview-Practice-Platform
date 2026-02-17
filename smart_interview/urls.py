9cf565b9182e80246888160be0459dfd6e5bb35f
Shefali Gupta <shefagup@amdocs.com>
2026-02-17 17:49:59 +0530

    Project Setup Completed

---
+++ smart_interview/urls.py
@@ -0,0 +1,12 @@
+"""
+URL configuration for Smart Interview Practice Platform.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
